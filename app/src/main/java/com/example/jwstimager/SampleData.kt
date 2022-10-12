76737e5cf906fbfd85a53e8e9333c209f859f59b
Declan Dury <declandury@gmail.com>
2022-10-12 02:36:40 -0700

    Start of UI using Android Compose

---
+++ app/src/main/java/com/example/jwstimager/SampleData.kt
@@ -0,0 +1,31 @@
+package com.example.jwstimager
+
+object SampleData {
+    val sampleImageList = listOf<ImageData>(
+        ImageData(
+            "JWST in Space",
+            "http://cdn.spacetelescope.org/archives/images/screen/jwst_in_space-cc.jpg",
+            R.drawable.jwst_in_space_cc
+        ),
+        ImageData(
+            "Test Image 1",
+            "Reddit.com",
+            R.drawable.testimage1
+        ),
+        ImageData(
+            "Test Image 1",
+            "Reddit.com",
+            R.drawable.testimage2
+        ),
+        ImageData(
+            "Test Image 1",
+            "Reddit.com",
+            R.drawable.testimage3
+        ),
+        ImageData(
+            "Test Image 1",
+            "Reddit.com",
+            R.drawable.testimage4
+        )
+    )
+}
