fe5f33ba082b10d763250be612e10c1207344480
Declan Dury <declandury@gmail.com>
2022-10-13 21:26:35 -0700

    Tried new colors

--- app/src/main/java/com/example/jwstimager/ui/theme/Color.kt
+++ app/src/main/java/com/example/jwstimager/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.jwstimager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Original Dark Theme Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+// Original Light Theme ColorsZ
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// From UI-resources discord channel
+val BlackOut = Color(0x202125FF) //TODO edit transparency on these colors
+val StretchLimo = Color(0x2a2b30DC)
+val AfterMidnight = Color(0x38393eDC)
+val AshleyBlue = Color(0x8899abC8)
--- app/src/main/java/com/example/jwstimager/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jwstimager/ui/theme/Theme.kt
@@ -16,9 +16,9 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlackOut,
+    secondary = AfterMidnight,
+    tertiary = AshleyBlue
 )
 
 private val LightColorScheme = lightColorScheme(
