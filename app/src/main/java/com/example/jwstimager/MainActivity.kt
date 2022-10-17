3cfb9f560d3378756e2f0b8b3f137c35f03e97fb
Emmanuel13g <emmanue.gavino757@myci.csuci.edu>
2022-10-16 19:24:37 -0700

    Share image from JWST Imager app

--- app/src/main/java/com/example/jwstimager/MainActivity.kt
+++ app/src/main/java/com/example/jwstimager/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.jwstimager
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
@@ -11,13 +19,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,14 +48,17 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
     }
 }
 
 data class ImageData(val title: String, val src_link: String, val rsc_id: Int)
 
+
 @Composable
 fun ImageCard(image: ImageData) {
 
+    val coroutineScope = rememberCoroutineScope()
     // keep track of whether or not the image card is expanded
     var isExpanded by remember { mutableStateOf(false) }
     // surfaceColor will be updated gradually from one color to the other
@@ -63,7 +75,9 @@ fun ImageCard(image: ImageData) {
             .animateContentSize()
             .padding(1.dp)
     ) {
+
         Column(modifier = Modifier.padding(all = 8.dp)) {
+
             Image(
                 painter = painterResource(id = image.rsc_id),
                 contentDescription = "This is a test image of the JWST",
@@ -74,6 +88,31 @@ fun ImageCard(image: ImageData) {
                     .clickable { isExpanded = !isExpanded }
                     .fillMaxSize()
             )
+
+            val context = LocalContext.current
+
+            Button(
+                onClick={
+
+                    context.sharing(image.rsc_id)
+                    /*
+                    val state = painter.state as? AsyncImagePainter.State.Success
+                    val drawable = state?.result?.drawable
+                    if (drawable != null) {
+                        context.shareImage(
+                            "Share image via",
+                            drawable,
+                            "testimage1"
+                        )
+                    }
+                    */
+
+
+                }) {
+                Text(text = "Share", color = Color.White)
+            }
+
+
             AnimatedVisibility(visible = isExpanded) {
                 //Spacer(modifier = Modifier.width(8.dp))
 
@@ -98,6 +137,24 @@ fun ImageCard(image: ImageData) {
         }
     }
 }
+
+fun Context.sharing(imageName: Int){
+    val b = BitmapFactory.decodeResource(resources, imageName)
+    val path = MediaStore.Images.Media.insertImage(contentResolver, b, "Image", null )
+
+    val uriPath = Uri.parse(path)
+
+    val shareIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, "From JWST Imager App")
+        type = "image/png"
+        putExtra(Intent.EXTRA_STREAM, uriPath)
+    }
+
+
+    startActivity(shareIntent)
+}
+
 
 @Composable
 fun ScrollingList(imageList: List<ImageData>) {
