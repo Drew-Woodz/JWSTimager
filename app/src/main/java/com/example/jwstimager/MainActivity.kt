fbf499e2a5fcbc729c6df995682b696834b12d70
Andrew Lockwood <andrew.lockwood578@myci.csuci.edu>
2022-11-05 15:15:46 -0700

    Attempt at Bottom Nav Bar

--- app/src/main/java/com/example/jwstimager/MainActivity.kt
+++ app/src/main/java/com/example/jwstimager/MainActivity.kt
@@ -2,15 +2,17 @@
 
 package com.example.jwstimager
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
-import androidx.compose.material.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -19,34 +21,24 @@ import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-
-import android.content.Context
-import android.content.Intent
-
-import android.graphics.BitmapFactory
-import android.net.Uri
-import android.os.Bundle
-import android.provider.MediaStore
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-
-
 import com.example.jwstimager.ui.theme.JWSTimagerTheme
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : ComponentActivity() {
@@ -69,7 +61,7 @@ fun MainScreen() {
     val navController = rememberNavController()
     Scaffold(
         topBar = { TitleBar() },
-        //bottomBar = { BottomNavigationBar(navController) },
+        bottomBar = { BottomNavigationBar(navController) },
         content = { padding ->
             Box(modifier = Modifier.padding(padding)) {
                 Navigation(navController = navController)
@@ -308,7 +300,7 @@ fun BottomNavigationBar(navController: NavHostController) {
         NavItem.News,
         NavItem.About
     )
-    BottomNavigation  (
+    BottomNavigation (
         backgroundColor = MaterialTheme.colorScheme.primary,
         contentColor = MaterialTheme.colorScheme.secondary,
     ) {
