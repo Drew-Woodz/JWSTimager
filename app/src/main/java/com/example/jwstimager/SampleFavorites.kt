027811710f8d66901e417349c038230cf582614d
Declan Dury <declandury@gmail.com>
2022-11-29 17:30:30 -0800

    removed old sample data

--- app/src/main/java/com/example/jwstimager/SampleFavorites.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jwstimager
-
-object SampleFavorites {
-    val sampleImageList = listOf<ImageData>(
-        ImageData(
-            "https://stsci-opo.org/STScI-01GEPRPYZKGEY9GT152DEK45GC.png"
-        )
-    )
-}
